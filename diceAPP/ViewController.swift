28cd388caa1d0bcfb81862b1bcd456e44d51b152
RubySun <sunru10075@gmail.com>
2022-12-20 20:14:32 +0800

    Initial Commit

---
+++ diceAPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  diceAPP
+//
+//  Created by 廖晨如 on 2022/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
